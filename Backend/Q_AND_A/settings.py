b08f637440a2f4ddc0c30a9f0d677e118a7e196e
followCode <vaishnavisinha2903@gmail.com>
2022-06-18 18:19:56 +0530

    Changes to the folder structure

--- Backend/Q_AND_A/settings.py
+++ Backend/Q_AND_A/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_o$r!w_(qsawypc()p-f-f4jkfyuc4ub-0p*ye8=*qosjy3s1#'
+SECRET_KEY = 'django-insecure-k-^ww3_e%11ur7q%#2$h)7z-a8+6h@(^3^pakow@p*5ehq9@5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
