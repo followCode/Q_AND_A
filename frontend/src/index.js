2e1fa161513c7f635c4298c245a213f89d51cfbe
1nF0rmed <prkinformed@gmail.com>
2022-10-09 14:42:42 +0530

    Added persistence to redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import storeFunction from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const {store, persistor} = storeFunction();
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { user } from './reducers/user';
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-export const store = configureStore({
-    reducer: {
-        user: user,
-    }
+const persistConfig = {
+    key: 'aquora',
+    storage,
+}
+
+const userReducers = combineReducers({
+    user: user,
 })
+
+const persistedReducer = persistReducer(persistConfig, userReducers)
+
+export default () => {
+    let store = configureStore({reducer: persistedReducer})
+    let persistor = persistStore(store)
+
+    return { store, persistor }
+}
